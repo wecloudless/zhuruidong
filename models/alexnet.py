fabeac5b92fcf6bc8b505e60b13d32205e52dc8c
horsebridge <1800011036@pku.edu.cn>
2023-07-06 19:07:37 +0800

    fix alexnet

--- models/alexnet.py
+++ models/alexnet.py
@@ -8,7 +8,7 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes: int = 100, dropout: float = 0.5) -> None:
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
